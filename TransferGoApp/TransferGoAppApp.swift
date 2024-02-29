7032d806d8e47b942f690333b3b3333153528f3b
Mikolaj Zawada <m.zawada@bsgroup.eu>
2024-02-29 11:28:31 +0100

    Initial Commit

---
+++ TransferGoApp/TransferGoAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  TransferGoAppApp.swift
+//  TransferGoApp
+//
+//  Created by Mikolaj Zawada on 29/02/2024.
+//
+
+import SwiftUI
+
+@main
+struct TransferGoAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
