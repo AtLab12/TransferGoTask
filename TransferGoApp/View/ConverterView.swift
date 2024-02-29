fad6e066feec10483579080f7faf18b67dc95729
Mikolaj Zawada <m.zawada@bsgroup.eu>
2024-02-29 11:30:50 +0100

    Initial work

---
+++ TransferGoApp/View/ConverterView.swift
@@ -0,0 +1,20 @@
+//
+//  ConverterView.swift
+//  TransferGoApp
+//
+//  Created by Mikolaj Zawada on 29/02/2024.
+//
+
+import Foundation
+import SwiftUI
+import UIKit
+
+struct ConverterView: View {
+    var body: some View {
+        Text("converter view")
+    }
+}
+
+#Preview {
+    ConverterView()
+}
